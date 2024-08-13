4f9019ef08134218f324fe25b8fcb2ee3c72e8a0
vladyslavskoryk <167355555+vladyslavskoryk@users.noreply.github.com>
2024-08-13 12:43:05 +0300

    Add ForecastIconWeatherDetails, shape purpleRoundedCorner, icons, colors. (#11)
    * Add ForecastIconWeatherDetails, shape purpleRoundedCorner, icons, colors.
    
    * Corrected UiElements.kt ForecastIconWeatherDetails.
    
    ---------
    
    Co-authored-by: VladyslavSkoryk <vladyslav.skoryk.ki.2021@lpnu.ua>

--- app/src/main/java/com/xwurfel/forecast/presentation/home/view/UiElements.kt
+++ app/src/main/java/com/xwurfel/forecast/presentation/home/view/UiElements.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -18,14 +19,20 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.xwurfel.forecast.R
+import com.xwurfel.forecast.ui.theme.BorderPurple
+import com.xwurfel.forecast.ui.theme.DarkPurple
 import com.xwurfel.forecast.ui.theme.ForecastTheme
-import com.xwurfel.forecast.ui.theme.purpleBubble
-import com.xwurfel.forecast.ui.theme.whiteBubble
+import com.xwurfel.forecast.ui.theme.PurpleBubble
+import com.xwurfel.forecast.ui.theme.TitleGray
+import com.xwurfel.forecast.ui.theme.WhiteBubble
 
 @Composable
 fun ForecastIconBubble(
@@ -38,10 +45,10 @@ fun ForecastIconBubble(
     Column(
         modifier = modifier
             .padding(vertical = 24.dp)
-            .background(color = purpleBubble, RoundedCornerShape(16.dp))
+            .background(color = PurpleBubble, RoundedCornerShape(16.dp))
             .border(
                 1.dp,
-                color = whiteBubble,
+                color = WhiteBubble,
                 shape = RoundedCornerShape(16.dp)
             ),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -71,6 +78,46 @@ fun ForecastIconBubble(
     }
 }
 
+@Composable
+fun ForecastIconWeatherDetails(
+    @DrawableRes icontitle: Int,
+    title: String,
+    modifier: Modifier = Modifier,
+    isActive: Boolean = false
+) {
+    Box(modifier = modifier) {
+        Column(
+            modifier = Modifier
+                .border(
+                    width = 1.dp, color = BorderPurple,
+                    shape = RoundedCornerShape(30.dp)
+                )
+                .size(140.dp)
+                .purpleRoundedCorner(shape = RoundedCornerShape(20))
+        ) {
+            Image(
+                painter = painterResource(id = icontitle),
+                contentDescription = null,
+                modifier = modifier
+                    .size(35.dp)
+                    .padding(vertical = 10.dp)
+            )
+            Text(
+                title,
+                color = TitleGray,
+                modifier = modifier
+                    .padding(horizontal = 25.dp, vertical = 5.dp)
+                    .size(35.dp)
+            )
+        }
+    }
+}
+
+fun Modifier.purpleRoundedCorner(shape: Shape) =
+    this then Modifier
+        .clip(shape = shape)
+        .background(color = DarkPurple)
+
 @Preview
 @Composable
 private fun PreviewForecastIconBubble() {
@@ -87,4 +134,21 @@ private fun PreviewForecastIconBubble() {
             )
         }
     }
+}
+
+@Preview
+@Composable
+private fun PreviewForecastIconWeatherDetails() {
+    ForecastTheme {
+        Surface(
+            color = Color.White,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            ForecastIconWeatherDetails(
+                title = "Title",
+                icontitle = R.drawable.feels_like_icon
+            )
+        }
+    }
+
 }
