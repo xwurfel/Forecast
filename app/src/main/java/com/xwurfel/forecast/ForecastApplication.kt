67ddeae0be3c51b81a2ae0e935c89aaefce942f2
vladyslavskoryk <167355555+vladyslavskoryk@users.noreply.github.com>
2024-09-02 19:14:21 +0300

    Feature/wf getlocation (#14)
    * Add LocationService.
    
    * Clean code.
    
    ---------
    
    Co-authored-by: VladyslavSkoryk <vladyslav.skoryk.ki.2021@lpnu.ua>

--- app/src/main/java/com/xwurfel/forecast/ForecastApplication.kt
+++ app/src/main/java/com/xwurfel/forecast/ForecastApplication.kt
@@ -1,7 +1,31 @@
 package com.xwurfel.forecast
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class ForecastApplication : Application()
+class ForecastApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                NOTIFICATION_CHANNEL_ID,
+                NOTIFICATION_CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_LOW
+            )
+            val notificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    companion object {
+        const val NOTIFICATION_CHANNEL_ID = "location"
+        const val NOTIFICATION_CHANNEL_NAME = "Location"
+    }
+}
--- app/src/main/java/com/xwurfel/forecast/presentation/home/view/HomeScreen.kt
+++ app/src/main/java/com/xwurfel/forecast/presentation/home/view/HomeScreen.kt
@@ -1,18 +1,25 @@
 package com.xwurfel.forecast.presentation.home.view
 
-import com.xwurfel.forecast.R
+import android.content.Intent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.xwurfel.forecast.LocationService
+import com.xwurfel.forecast.R
 import com.xwurfel.forecast.domain.model.weather_forecast.WeatherForecast
 import com.xwurfel.forecast.presentation.common.CircularLoadingIndicator
 import com.xwurfel.forecast.presentation.common.GenericErrorScreen
@@ -26,14 +33,36 @@ fun HomeScreen(
     viewModel: HomeViewModel = hiltViewModel(),
 ) {
     val uiState = viewModel.uiState.collectAsStateWithLifecycle().value
-
+    val applicationContext = LocalContext.current.applicationContext
     when (uiState) {
         is Loading -> {
             CircularLoadingIndicator()
         }
 
         is Success -> {
-            HomeScreenContent(uiState.data)
+            Column(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Spacer(modifier = Modifier.height(50.dp))
+                Button(onClick = {
+                    Intent(applicationContext, LocationService::class.java).apply {
+                        action = LocationService.ACTION_START
+                        applicationContext.startService(this)
+                    }
+                }) {
+                    Text(text = "Start")
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+                Button(onClick = {
+                    Intent(applicationContext, LocationService::class.java).apply {
+                        action = LocationService.ACTION_STOP
+                        applicationContext.startService(this)
+                    }
+                }) {
+                    Text(text = "Stop")
+                }
+                //    HomeScreenContent(uiState.data)
+            }
         }
 
         is Error -> {
