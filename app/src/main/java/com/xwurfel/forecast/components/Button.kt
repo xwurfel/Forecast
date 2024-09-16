60c7d6a6a42571ef0ef70bf0abffd06a0189eda0
VladyslavSkoryk <vladyslav.skoryk.ki.2021@lpnu.ua>
2024-09-16 18:10:11 +0300

    Add RoundPlusButton & plus_icon.png

--- app/src/main/java/com/xwurfel/forecast/components/Button.kt
+++ app/src/main/java/com/xwurfel/forecast/components/Button.kt
@@ -1,35 +1,24 @@
 package com.xwurfel.forecast.components
 
-import android.graphics.drawable.Icon
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.xwurfel.forecast.R
-import com.xwurfel.forecast.presentation.common.CircularLoadingIndicator
 import com.xwurfel.forecast.ui.theme.BorderPurple
 import com.xwurfel.forecast.ui.theme.ForecastTheme
 
